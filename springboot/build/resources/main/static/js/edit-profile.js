2874b0fe891372b0967e508b4bb524c68fe453da
jaey-oung <jaeywodud7@gmail.com>
2025-04-23 17:33:56 +0900

    chore: .gitignore 복구 테스트 3

--- springboot/build/resources/main/static/js/edit-profile.js
+++
@@ -1,9 +0,0 @@
-const deleteBtn = document.getElementById('delete-user');
-
-deleteBtn.addEventListener('click', function (event) {
-    const identify = confirm('정말로 회원 탈퇴하시겠습니까?');
-
-    if (!identify) {
-        event.preventDefault();
-    }
-});
