32b77c35fb9f753af2d8bfe0b01a11761aa93588
18hailey <hailey9459@gmail.com>
2025-04-24 11:43:54 +0900

    refactor: bookList 메서드 프론트에서 받아오는 매개변수 변경, 카테고리 경로도 바로 받아 모델 객체에 추가
    - 도서 리스트 출력 메서드 정렬 조건 내부에서 처리하여 하나씩만 남겨두기
    - 빠뜨린 출판사, 저자 정보도 받아오기

--- springboot/src/main/java/com/wru/wrubookstore/controller/BookController.java
+++ springboot/src/main/java/com/wru/wrubookstore/controller/BookController.java
@@ -1,24 +1,20 @@
 package com.wru.wrubookstore.controller;
 
 import com.wru.wrubookstore.domain.PageHandler;
-import com.wru.wrubookstore.domain.MainSearchCondition;
-import com.wru.wrubookstore.domain.SearchCondition;
+import com.wru.wrubookstore.domain.HomeSearchCondition;
 import com.wru.wrubookstore.dto.*;
+import com.wru.wrubookstore.dto.request.BookSearchRequestDto;
+import com.wru.wrubookstore.dto.request.category.CategoryRequest;
 import com.wru.wrubookstore.dto.response.category.CategoryResponse;
-import com.wru.wrubookstore.dto.response.publisher.PublisherListResponse;
 import com.wru.wrubookstore.dto.response.review.ReviewListResponse;
-import com.wru.wrubookstore.dto.response.writer.WriterListResponse;
 import com.wru.wrubookstore.service.*;
-import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
-import java.io.Writer;
 import java.util.*;
 
 @Controller
@@ -40,39 +36,32 @@ public class BookController {
 
     /* 메인 홈페이지 메뉴의 카테고리 클릭 시 도서 리스트 출력 */
     @GetMapping("/bookList")
-    public String bookList(MainSearchCondition sc, Model model, HttpServletRequest request, Integer sort) {
+    public String bookList(HomeSearchCondition sc,
+                           @RequestParam("cl_id") String categoryLargeId,
+                           @RequestParam("cm_id") String categoryMediumId,
+                           @RequestParam("cs_id") String categorySmallId,
+                           @RequestParam("path") String path,
+                           Model model, HttpServletRequest request) {
+
+        // 대/중/소분류 카테고리 id를 하나의 객체로 관리
+        CategoryRequest categoryIds = new CategoryRequest(categoryLargeId, categoryMediumId, categorySmallId);
 
         try{
-            if(sort == null){
-                sort = 0;
-            }
-            // 도서 테이블에서 특정 카테고리에 속한 도서 개수 파악
-            int count = bookService.selectByCategoryCnt(sc.getCategory());
-            List<CategoryDto> list = new ArrayList<>();
+            // 해당 카테고리에 속한 도서 개수 구하기
+            int count = bookService.getCntByCategoryIds(categoryIds);
 
-            // 개수가 0이면 카테고리만 조인한 테이블에서 카테고리 정보만 반환
-            if (count == 0) {
-                CategoryDto categoryInfo = bookService.selectCategoryInfo(sc.getCategory());
-                model.addAttribute("category", categoryInfo);
-            } else if (count > 0) {
-                // 개수가 1 이상이면 도서 테이블에서 특정 카테고리에 속한 도서 개수 및 도서 리스트, 카테고리 정보 반환
-                if(sort == 0){
-                    list = bookService.selectByCategory(sc);
-                }
-                else if(sort == 1){
-                    list = bookService.selectByCategory2(sc);
-                }
-                else if(sort == 2){
-                    list = bookService.selectByCategory3(sc);
-                }
-            } else {
-                throw new Exception("잘못된 도서 개수입니다.");
+            List<CompleteBookDto> list = new ArrayList<>();
+            if (count != 0) {
+                // 페이징, 정렬과 관련된 sc와 카테고리 id를 조합해 조건에 맞는 도서 리스트 반환
+                BookSearchRequestDto bookSearchRequestDto = new BookSearchRequestDto(sc, categoryIds);
+                list = bookService.getAllCompleteBooks(bookSearchRequestDto); // 책, 출판사, 저자 정보 포함
             }
 
             model.addAttribute("sc", sc);
             model.addAttribute("list", list);
             model.addAttribute("ph", new PageHandler(count, sc.getPage(), sc.getPageSize()));
-            model.addAttribute("uri", request.getRequestURI()); // 페이징 시 해당 uri 정보 전달
+            model.addAttribute("path", path);
+            model.addAttribute("url", request.getRequestURI()); // 페이징 시 해당 url 정보 전달
         } catch(Exception e) {
             e.printStackTrace();
         }
